e7fb28a91354d1c6cd5e6e2a9da2814b0ee104cc
paras9sid <sidharthjainp9@gmail.com>
2023-05-25 21:59:09 +0530

    button functionality number & display dynamic set.

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//checking app.js synced properly with html file
-// alert("working");
-
 //main
 
 //button assigned
@@ -11,54 +8,75 @@ const p2Button = document.querySelector("#p2Button");
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 
+//reset button
+const resetButton = document.querySelector("#reset");
+
+//select winnig score limit options
+const winningScoreSelect = document.querySelector("#playTo");
+
 //default score of players
 let p1Score = 0;
 let p2Score = 0;
 
 //winning score points limit
-let winningScore = 5;
+let winningScore = 3;
 
 //boolean call for game over
 let isGameOver = false;
 
 p1Button.addEventListener("click", (e) => {
-  //   alert("p1 button working"); // check button working.
-
   //game over condition check
   if (!isGameOver) {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
+      //adding classes for number to change color for winner and loser
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
-
-  //   //condition of player 1 win
-  //   if (p1Score !== winningScore) {
-  //     // will limit score to default winning score = 5 not beyond that
-  //     //changing p1 score text and counting increase
-  //     p1Score += 1;
-  //     p1Display.textContent = p1Score;
-  //   }
 });
 
 p2Button.addEventListener("click", (e) => {
-  //   alert("p1 button working");
-
   //   game over condition check
   if (!isGameOver) {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
+      //adding classes for number to change color for winner and loser
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
     p2Display.textContent = p2Score;
   }
-
-  //   //condition of player 1 win
-  //   if (p2Score !== winningScore) {
-  //     // will limit score to default winning score = 5 not beyond that
-  //     //changing p1 score text and counting increase
-  //     p2Score += 1;
-  //     p2Display.textContent = p2Score;
-  //   }
 });
+
+//winning score limit functionality
+winningScoreSelect.addEventListener("change", () => {
+  //   alert("Change"); // check working
+  //   alert(this.value); // undefined printing in alert - t check
+
+  winningScore = parseInt(this.value);
+  reset();
+});
+
+//reset funtion -
+const reset = (e) => {
+  //values to bring back down to initial so to start game again.
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+
+  //display updation to initial values again upon clicking button
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+
+  //ADDING WINNER & LOSER classes
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+};
+
+//after reset funtion only above written it will work
+//reset button functionality = any number to 0 again
+resetButton.addEventListener("click", reset);
